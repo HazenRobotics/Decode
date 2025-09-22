1e23032b75191f7d4ae75d49dc8f578ea970f58c
Armaan Sidhu <armaansidhu000@icloud.com>
2025-09-22 15:33:09 -0700

    Base code for StarterRobot, issues with rotating and forgot to add two servos in code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/StarterRobot.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.SubSystems.Shooter;
+import org.firstinspires.ftc.teamcode.SubSystems.TankDrive;
+import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
+
+public class StarterRobot {
+    TankDrive drive;
+    Shooter launcher;
+    GamepadEvents controller1, controller2;
+    public StarterRobot(HardwareMap hw, GamepadEvents controller1, GamepadEvents controller2)
+    {
+        drive = new TankDrive(hw);
+        launcher = new Shooter(hw, "leftShooter");
+        this.controller1 = controller1;
+        this.controller2 = controller2;
+
+    }
+
+    public void drive()
+    {
+        drive.drive(controller1.left_stick_y, controller1.left_stick_x);
+    }
+
+    public void shoot()
+    {
+        launcher.shoot(controller1.left_trigger.getTriggerValue() -controller1.right_trigger.getTriggerValue());
+    }
+}
