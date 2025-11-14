f9888b916d4542cbb29ed8550af25d284b104861
Joseph <jingshunceng@gmail.com>
2025-11-13 18:35:00 -0800

    Pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2Auto/BluePedroAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2Auto/BluePedroAuto.java
@@ -26,15 +26,15 @@ public class BluePedroAuto extends LinearOpMode {
     private final Pose shootingPose = new Pose(46.33043478260869,96.83478260869565,Math.toRadians(135));
     //Near side 3 balls
     private final Pose firstLine = new Pose(41.321739130434786,35.686956521739134,Math.toRadians(0));
-    private final Pose firstPush = new Pose(14.191304347826087,84.10434782608695,Math.toRadians(0));
+    private final Pose firstPush = new Pose(18.782608695652172,84.10434782608695,Math.toRadians(0));
     private final Pose firstControl = new Pose(51.547826086956526,80.76521739130435);
     //Middle 3 balls
     private final Pose secondLine = new Pose(40.904347826086955,60.313043478260866,Math.toRadians(0));
-    private final Pose secondPush = new Pose(14.191304347826087,60.104347826086965,Math.toRadians(0));
+    private final Pose secondPush = new Pose(18.782608695652172,60.104347826086965,Math.toRadians(0));
     private final Pose secondControl = new Pose(61.982608695652175,54.469565217391306);
     //Last three balls
     private final Pose thirdLine = new Pose(40.48695652173913,84.31304347826088,Math.toRadians(0));
-    private final Pose thirdPush = new Pose(14.817391304347826,83.89565217391304,Math.toRadians(0));
+    private final Pose thirdPush = new Pose(18.782608695652172,83.89565217391304,Math.toRadians(0));
     private final Pose thirdControl = new Pose(63.02608695652174,28.382608695652173);
     private PathChain shoot, firstBall, push1, back1, secondBall, push2, back2, thirdBall, push3, back3;
     public void buildPaths(){
@@ -44,8 +44,10 @@ public class BluePedroAuto extends LinearOpMode {
                 .build();
 
         firstBall = follower.pathBuilder()
-                .addPath(new BezierCurve(shootingPose,firstControl, firstPush))
+                .addPath(new BezierLine(shootingPose, firstLine))
                 .setLinearHeadingInterpolation(Math.toRadians(135),Math.toRadians(0))
+                .addPath(new BezierLine(firstLine,firstPush))
+                .setConstantHeadingInterpolation(Math.toRadians(0))
                 .build();
 
         back1 = follower.pathBuilder()
@@ -54,8 +56,10 @@ public class BluePedroAuto extends LinearOpMode {
                 .build();
 
         secondBall = follower.pathBuilder()
-                .addPath(new BezierCurve(shootingPose,secondControl, secondPush))
+                .addPath(new BezierLine(shootingPose, secondLine))
                 .setLinearHeadingInterpolation(Math.toRadians(135),Math.toRadians(0))
+                .addPath(new BezierLine(secondLine,secondPush))
+                .setConstantHeadingInterpolation(Math.toRadians(0))
                 .build();
 
         back2 = follower.pathBuilder()
@@ -64,8 +68,10 @@ public class BluePedroAuto extends LinearOpMode {
                 .build();
 
         thirdBall = follower.pathBuilder()
-                .addPath(new BezierCurve(shootingPose,thirdControl, thirdPush))
+                .addPath(new BezierLine(shootingPose, thirdLine))
                 .setLinearHeadingInterpolation(Math.toRadians(135),Math.toRadians(0))
+                .addPath(new BezierLine(thirdLine,thirdPush))
+                .setConstantHeadingInterpolation(Math.toRadians(0))
                 .build();
 
         back3 = follower.pathBuilder()
